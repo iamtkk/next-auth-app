5c3430e3ba91bcdb85ead7175faacfad9c5c3943
김태관 <iamtk@neowiz.com>
2024-10-06 23:47:50 +0900

    email reset complete

--- app/(logged-out)/password-reset/actions.ts
+++ app/(logged-out)/password-reset/actions.ts
@@ -4,6 +4,7 @@ import { auth } from "@/auth";
 import db from "@/db/drizzle";
 import { passwordResetTokens } from "@/db/passwordResetTokenSchema";
 import { users } from "@/db/usersSchema";
+import { mailer } from "@/lib/email";
 import { randomBytes } from "crypto";
 import { eq } from "drizzle-orm";
 
@@ -45,4 +46,13 @@ export const passwordReset = async (emailAddress: string) => {
         tokenExpiry,
       },
     });
+
+  const resetLink = `${process.env.SITE_BASE_URL}/update-password?token=${passwordResetToken}`;
+
+  await mailer.sendMail({
+    from: "test@resend.dev",
+    subject: "Your password reset request",
+    to: emailAddress,
+    html: `Jey, ${emailAddress}! You requested to reset your password. Here's your password reset link. This link will expire in 1 hour: <a href="${resetLink}">${resetLink}</a>`,
+  });
 };
